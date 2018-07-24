b60f072dd7577dd87818a002c1285a971285aea8
Doiinn <rpg2xjob.doiinn@gmail.com>
2018-07-25 06:02:08 +0700

    Update data and add scrollreveal

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,18 +1,19 @@
 <template>
+  <div class="profile">
     <div class="container">
-
-  <div class="row">
-    <div class="column">
-      <h2>Profile</h2>
+      <div class="row">
+        <div class="column">
+          <h2>Profile</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="column">
+          <div class="detail">Name: Worapong Malaiwong</div>
+          <div class="detail">Education: King Mongkut's Institute of Technology Ladkrabang</div>
+          <div class="sub-detail">Bachelor of Science (Information Technology)</div>
+        </div>
+      </div>
     </div>
-  </div>
-
-   <div class="row">
-    <div class="column">
-      <h3>Name: Worapong Malaiwong</h3>
-    </div>
-  </div>
-
   </div>
 </template>
 
@@ -23,14 +24,22 @@ export default {
 </script>
 
 <style scoped lang="scss">
-  $eng_font: 'PT Mono', monospace;
+$eng_font: "PT Mono", monospace;
 
-  h2 {
-    font-family: $eng_font;
-    text-align: center;
-  }
+h2 {
+  font-family: $eng_font;
+  text-align: center;
+  font-size: 2.25em;
+}
 
-  h3 {
-    font-family: $eng_font;
-  }
+.detail {
+  font-family: $eng_font;
+  font-size: 1.4em;
+}
+
+.sub-detail {
+  font-family: $eng_font;
+  font-size: 1.2em;
+  margin-left: 7.8em;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="home">
     <Header/>
     <Profile/>
   </div>
@@ -9,12 +9,24 @@
 // @ is an alias to /src
 import Header from '@/components/Header.vue'
 import Profile from '@/components/Profile.vue'
+import ScrollReveal from 'scrollreveal'
+
+const sr = ScrollReveal({ reset: false })
 
 export default {
   name: 'home',
   components: {
     Header,
     Profile
+  },
+  mounted: () => {
+    sr.reveal('.profile', { duration: 1500, easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)', scale: 0.5 })
   }
 }
 </script>
+
+<style scoped lang="scss">
+  #home {
+    padding-bottom: 8em;
+  }
+</style>
