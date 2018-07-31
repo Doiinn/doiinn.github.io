35e3ea3858398d1b83e11e35361c9b7f2474fa9a
Doiinn <rpg2xjob.doiinn@gmail.com>
2018-07-31 11:40:34 +0700

    Web page files

---
+++ precache-manifest.502714d91bd071dd7e3b01c126683b64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "65124aa0a8a5cd9441a2",
+    "url": "/js/chunk-vendors.65124aa0.js"
+  },
+  {
+    "revision": "55b41e70c95b8578c63e",
+    "url": "/js/app.55b41e70.js"
+  },
+  {
+    "revision": "6ddad85873ab0ebb2dd38ba0f7959690",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55b41e70c95b8578c63e",
+    "url": "/css/app.5c90ea9b.css"
+  }
+];
